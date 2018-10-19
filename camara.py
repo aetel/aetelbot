3efc272d8348ae6162348dea75c825ce2061db03
Ricardo <me@ricardosa.nz>
2018-10-19 09:39:34 +0200

    Camara now works

--- camara.py
+++ camara.py
@@ -7,11 +7,13 @@ settings = DataLoader()
 
 def foto(bot, update, args, job_queue, chat_data):
     log_message(update)
-    logging.debug('Directorio imágenes: '+settings.pictures_directory )
-
-def take_http_screenshot():
-    os.system('rm ' + settings.pictures_directory + '/snapshot.jpg')
-    os.system('wget --output-document ' + settings.pictures_directory + '/snapshot.jpg ' + settings.cam_url)
+    logging.info('Enviando foto...')
+    logging.debug('Directorio imágenes: ' + settings.pictures_directory + '/image.jpg')
+    os.system('wget -nv --output-document ' + pictures_directory + '/image.jpg ' + settings.cam_url)
+    photo_message = bot.send_photo(chat_id=chat_id, photo=open('./'+settings.pictures_directory + '/image.jpg', 'rb'))
+    os.system('rm ' + settings.pictures_directory + '/image.jpg')
+    job = job_queue.run_once(deleteMessage, 5, context=photo_message.message_id)
+    job = job_queue.run_once(deleteMessage, 5, context=update.message.message_id)
 
 if __name__ == "__main__":
     print ("Bienvenido, se va a aser una fotico.")
