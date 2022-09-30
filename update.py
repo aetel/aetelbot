d4566f0d400f39695a33252e7b40c1e754c2a9e7
Alejandro Martínez <alexrioja98@gmail.com>
2022-09-30 18:46:25 +0200

    removed old files

--- update.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import sys, os
-
-def update_bot():
-    os.system('sh update-aetelbot.sh')
-
-if __name__ == "__main__":
-    print ("A tomar por saco el bot.")
